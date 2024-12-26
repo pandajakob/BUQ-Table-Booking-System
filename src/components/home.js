77994a4183fafc7808b98b5278f58e990ddafe4c
pandajakob <jakobm.dev@gmail.com>
2024-12-26 19:35:56 +0100

    changes to homepage

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,9 @@
 export const HomePage = () => {
   return (
     <div className="flex">
-        <h1> BUQ </h1>
-        <h3> Table booking system </h3>
+        <h1> BUQ </h1> <br/>
+        <h3> Table booking system </h3> <br/>
+        <p> copyright</p>
     </div>
   );
 };
