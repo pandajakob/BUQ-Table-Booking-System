d6b0785778d680d50ef662e3207ea2bac802ef15
pandajakob <jakobm.dev@gmail.com>
2024-12-26 20:00:44 +0100

    change home.js

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,6 @@ export const HomePage = () => {
     <div className="flex">
         <h1> BUQ </h1> <br/>
         <h3> Table booking system </h3> <br/>
-        <p> copyright</p>
     </div>
   );
 };
